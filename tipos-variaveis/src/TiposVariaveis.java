d7a421774e37a162a4865909b10239d4a0fd32f5
Arthur I Borges <arthuriborges100@gmail.com>
2024-05-10 21:27:08 -0300

    adicionando os exemplos das aulas sobre tipos de variaveis

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,19 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+
+        double salario = 2500; // 2 mil e quinhentos reais
+        double salario2 = 2.500; // 2 reais
+
+        short numCurto = 1;
+        int numNormal = numCurto;
+        //short numCurto2 = numNormal;
+        // erro, "short" é menor que "int"
+        short numCurto2 = (short) numNormal;
+        // CASTING - conversão de objetos ou tipos primitivos de um tipo para outro tipo
+    
+        final double VALOR_DE_PI = 3.14;
+        // declarar uma variavel fixa: Caixa alta e adicionar "final"
+
+        System.out.println(numCurto2);
     }
 }
